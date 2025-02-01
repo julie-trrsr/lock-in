21a1cd3e150d939d955a43d72760cbe09dec6aec
Julie <julie.terrassier@epfl.ch>
2025-02-01 17:05:14 +0000

    Feat: user button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,19 @@
 "use client"
 import Image from "next/image";
 import { useState } from "react";
+import { FaUser } from "react-icons/fa"
+import { CiUser, CiUnlock, CiLock } from "react-icons/ci";
 
 export default function Home() {
   const [lockedIn, setLockedIn] = useState(false);
 
   return (
     <div className="flex flex-1 items-center justify-center justify-items-center min-h-screen p-8 sm:p-20">
+      <div className="absolute top-0 right-0 rounded-full hover:bg-neutral-300 hover:cursor-pointer mt-4 mr-4">
+        <FaUser className="w-12 h-12 p-2 text-black"/>
+      </div>
       <div className="flex flex-col gap-8 row-start-2 items-center">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <CiUnlock className="w-20 h-20 hover:cursor-pointer" />
         <div className="text-5xl text-center sm:text-left">
           Lock in!
         </div>

1c39cb5d89db314309bb207e697f9269c6c93dd3
maria <mariasmirnova715@gmail.com>
2025-02-02 01:48:06 +0000

    Chore: improve NOTE/TODO in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-from database.tools import add_log, add_message, add_user, get_messages_per_log, get_user_id_from_log, get_user_infos_from_username, setup_database
+from database.tools import setup_database
 
 DATABASE_PATH = "src/database/database.db"
 
@@ -11,7 +11,9 @@ try:
         #NOTE: Do we enable foreign key constraints?
         #conn.execute("PRAGMA foreign_keys = ON")
 
-        #NOTE: use conn.commit() each time we modify the database
+        #NOTE: !!!!!! use conn.commit() each time we modify the database !!!!!!
+
+        #TODO: handle erros
 
 
 except sqlite3.OperationalError as e:
